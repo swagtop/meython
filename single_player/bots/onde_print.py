e25906c6d4ef5d48f5da2ed27ba65eeb2b00c209
thedb <tcj841@alumni.ku.dk>
2024-03-06 15:57:57 +0100

    Restructuring

--- single_player/bots/onde_print.py
+++
@@ -1,4 +0,0 @@
-def answer(previous: int, roll: int) -> int:
-    if previous == 4:
-        return roll
-    return 41
--- single_player/bots/print.py
+++
@@ -1,6 +0,0 @@
-def answer(previous: int, roll: int) -> int:
-    if previous == 4:
-        return roll
-    else:
-        return 1
-
